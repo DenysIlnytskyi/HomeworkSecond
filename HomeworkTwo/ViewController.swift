e733ea1a4ef2a3aac0e794f31d49b83e6375a928
DenysIlnytskyi <380984511271k@gmail.com>
2022-01-13 18:43:43 +0100

    Completed

--- HomeworkTwo/ViewController.swift
+++ HomeworkTwo/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var valueOfRedSlider: UILabel!
+    @IBOutlet var valueOfGreenSlider: UILabel!
+    @IBOutlet var valueOfBlueSlider: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        
+        valueOfRedSlider.text = String(format: "%.2f", redSlider.value)
+        valueOfGreenSlider.text = String(format: "%.2f", greenSlider.value)
+        valueOfBlueSlider.text = String(format: "%.2f", blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    
     }
-
+    @IBAction func mixSliderValue(_ sender: UIColor) {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    
+        valueOfRedSlider.text = String(format: "%.2f", redSlider.value)
+        valueOfGreenSlider.text = String(format: "%.2f", greenSlider.value)
+        valueOfBlueSlider.text = String(format: "%.2f", blueSlider.value)
+    }
+    
 
 }
 
